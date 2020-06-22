e2de72c6e173f2c7331ccfc440669218557240f4
Dane Trbhyhozborac <nemanjapipi@hotmail.com>
2020-06-22 13:48:16 +0200

    Done few functionalities for threadOne and threadTwo

--- Dan_XXXVI_Nemanja_Pilipovic/Zadatak_1/Models/App.cs
+++ Dan_XXXVI_Nemanja_Pilipovic/Zadatak_1/Models/App.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 
 namespace Zadatak_1.Models
@@ -10,6 +11,8 @@ namespace Zadatak_1.Models
         static List<int> randomNumbers = new List<int>();
         static Random random = new Random();
         static Object obj = new Object();
+        static readonly string location = @"~\..\..\..\OddNumbers.txt";
+        
 
         public void Start()
         {
@@ -21,7 +24,15 @@ namespace Zadatak_1.Models
             Thread threadOne = new Thread(() => CreateMatrix());
             Thread threadTwo = new Thread(() => PopulateMatrix());
             threadOne.Start();
-            threadTwo.Start();            
+            threadTwo.Start();
+            threadTwo.Join();
+
+            Thread threadThree = new Thread(() => GetOddNumbers());
+            threadThree.Start();
+            threadThree.Join();
+
+            Thread threadFour = new Thread(() => WriteFromFile());
+            threadFour.Start();
         }
 
         private void CreateMatrix()
@@ -53,6 +64,60 @@ namespace Zadatak_1.Models
             int randomNumber = random.Next(10, 100);
             randomNumbers.Add(randomNumber);
             return randomNumber;            
+        }
+
+        private void GetOddNumbers()
+        {
+            List<int> oddNumbers = new List<int>();
+            for (int i = 0; i < 10000; i++)
+            {
+                if(randomNumbers[i] % 2 == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    oddNumbers.Add(randomNumbers[i]);
+                }
+            }
+            int[] oddNumbersArray = new int[oddNumbers.Count];
+            for (int i = 0; i < oddNumbers.Count; i++)
+            {
+                oddNumbersArray[i] = oddNumbers[i];
+            }
+            if (File.Exists(location))
+            {
+                using (StreamWriter sw = new StreamWriter(location))
+                {
+                    foreach (int number in oddNumbersArray)
+                    {
+                        sw.WriteLine(number);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+                Environment.Exit(0);
+            }
+        }
+
+
+        private void WriteFromFile()
+        {
+            if (File.Exists(location))
+            {
+                using (StreamReader sr = new StreamReader(location))
+                {
+                    string numbers = sr.ReadToEnd();
+                    Console.WriteLine(numbers);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+                Environment.Exit(0);
+            }
         }
     }
 }
